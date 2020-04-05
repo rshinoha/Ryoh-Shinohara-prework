fe0c622cbcc3529c926e80bb4f47f240eb23e486
rshinoha <r.shinohara@zoho.com>
2020-04-04 20:37:14 -0500

    finished event listener-related coding

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,17 +1,17 @@
 // List of pictures
 const pictures = {
     ver01: {
-        picture: '../images/ver01.jpg',
+        picture: 'assets/images/ver01.jpg',
         badge: '<a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@mikeshen?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from shen liu"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">shen liu</span></a>',
         name: 'Verstappen'
     },
     hul01: {
-        picture: '../images/hul01.jpg',
+        picture: 'assets/images/hul01.jpg',
         badge: '<a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@chuttersnap?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from chuttersnap"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">chuttersnap</span></a>',
         name: 'Hulkenberg'
     },
     rai01: {
-        picture: '../images/rai01.jpg',
+        picture: 'assets/images/rai01.jpg',
         badge: '<a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@jamesbo13?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from James Bowen"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">James Bowen</span></a>',
         name: 'Raikkonen'
     }
@@ -20,11 +20,20 @@ const pictures = {
 // Symbol for blank word
 const symbol = '_';
 
+// Correct or incorrect answers
+const checkMark = '<svg class="bi bi-check" width="1em" height="1em" viewBox="0 0 16 16" fill="green" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/></svg>';
+const xMark = '<svg class="bi bi-x" width="1em" height="1em" viewBox="0 0 16 16" fill="red" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 010 .708l-7 7a.5.5 0 01-.708-.708l7-7a.5.5 0 01.708 0z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 000 .708l7 7a.5.5 0 00.708-.708l-7-7a.5.5 0 00-.708 0z" clip-rule="evenodd"/></svg>';
+
+// Directions at start and end of game
+const dirGameStart = 'Press any key to get started!';
+const dirCorrect = 'You\'re correct! Press enter to start a new game.';
+const dirWrong = 'Sorry! You\'ve used up all of your guesses. Press enter to start a new game.';
+
 // List of picture names
 const picNames = Object.keys(pictures);
 
 // Max number of guesses available
-const maxGuess = 10;
+const maxGuess = 5;
 
 // Chooses random picture name
 const choosePic = function(arr) {
@@ -70,11 +79,18 @@ const replaceBlanks = function(word, blanks, letter) {
     return updateBlanks;
 }
 
+// Checks if game is finished
+const gameFinished = function() {
+    return !blanks.innerText.includes(symbol) || currentCount <= 0;
+}
+
 // Updates blanks with appropriate letters
 const updateGame = function(event) {
-    blanks.innerText = replaceBlanks(randName, blanks.innerText, event.key);
-    updateGuess(event);
-    updateUsedWords(event);
+    if(!gameFinished()) {
+        blanks.innerText = replaceBlanks(randName, blanks.innerText, event.key);
+        updateGuess(event);
+        updateUsedWords(event);
+    }
 }
 
 // Checks if letter has been used or is a letter included in the word
@@ -102,20 +118,61 @@ const updateUsedWords = function(event) {
     }
 }
 
+const updateMarks = function(event) {
+    if(gameFinished()) {
+        if(!blanks.innerText.includes(symbol)) {
+            correct.innerHTML = checkMark;
+            directions.innerText = dirCorrect;
+        }
+        else {
+            correct.innerHTML = xMark;
+            directions.innerText = dirWrong;
+        }
+        console.log(event.key);
+    }
+}
+
+const newGame = function(event) {
+    const key = event.key;
+    if(key === 'Enter') {
+        randPic = choosePic(picNames);
+        randName = pictures[randPic]["name"].toUpperCase();
+        console.log(randName);
+        currentCount = maxGuess;
+        directions.innerText = dirGameStart;
+        picture.src = pictures[randPic]["picture"];
+        badge.innerHTML = pictures[randPic]["badge"];
+        currentBlanks = makeBlanks(randName);
+        blanks.innerText = currentBlanks;
+        guessCount.innerText = currentCount;
+        usedLetters.innerText = '';
+        correct.innerHTML = '';
+    }
+}
+
 // Choosing random picture
 let randPic = choosePic(picNames);
 let randName = pictures[randPic]["name"].toUpperCase();
 console.log(randName);
 
 // References
+let directions = document.querySelector('#directions');
+let picture = document.querySelector('#picture');
+let badge = document.querySelector('#badge');
 let blanks = document.querySelector('#blanks');
 let guessCount = document.querySelector('#guessCount');
 let usedLetters = document.querySelector('#usedLetters');
 let currentCount = maxGuess;
+let correct = document.querySelector('#isCorrect');
 
+directions.innerText = dirGameStart;
+picture.src = pictures[randPic]["picture"];
+badge.innerHTML = pictures[randPic]["badge"];
 currentBlanks = makeBlanks(randName);
 blanks.innerText = currentBlanks;
 guessCount.innerText = currentCount;
 usedLetters.innerText = '';
 
 document.addEventListener('keyup', updateGame);
+document.addEventListener('keyup', updateMarks);
+document.addEventListener('keyup', newGame);
