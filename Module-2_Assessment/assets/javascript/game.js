5612cd53ca5231e168437601837a42bd865b33ef
rshinoha <r.shinohara@zoho.com>
2020-04-04 19:22:55 -0500

    added more functionalities in game.js

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -20,6 +20,12 @@ const pictures = {
 // Symbol for blank word
 const symbol = '_';
 
+// List of picture names
+const picNames = Object.keys(pictures);
+
+// Max number of guesses available
+const maxGuess = 10;
+
 // Chooses random picture name
 const choosePic = function(arr) {
     const randNum = Math.floor(Math.random() * arr.length);
@@ -28,8 +34,23 @@ const choosePic = function(arr) {
 
 // Generates string of blanks
 const makeBlanks = function(word) {
-    const numBlanks = word.length;
-    return symbol.repeat(numBlanks);
+    let numBlanks = 0;
+    let strBlanks = '';
+    for( let i = 0; i < word.length; i++ ) {
+        if(isLetter(word[i])) {
+            strBlanks += symbol;
+        }
+        else {
+            strBlanks += word[i];
+        }
+    }
+    return strBlanks;
+}
+
+// Checks if character is part of the alphabet
+// Provided by this Stack Overflow anser: https://stackoverflow.com/a/32567789
+const isLetter = function(c) {
+    return c.toLowerCase() != c.toUpperCase();
 }
 
 // Replaces blanks with correct letters
@@ -52,24 +73,49 @@ const replaceBlanks = function(word, blanks, letter) {
 // Updates blanks with appropriate letters
 const updateGame = function(event) {
     blanks.innerText = replaceBlanks(randName, blanks.innerText, event.key);
+    updateGuess(event);
+    updateUsedWords(event);
 }
 
-// List of picture names
-const picNames = Object.keys(pictures);
+// Checks if letter has been used or is a letter included in the word
+const addToUsedLetter = function(c) {
+    let letter = c.toUpperCase();
+    return isLetter(letter) && !randName.includes(letter) && !usedLetters.innerText.includes(letter) && letter.length == 1;
+}
 
-// Max number of guesses available
-const maxGuess = 10;
+const updateGuess = function(event) {
+    if(addToUsedLetter(event.key)) {
+        currentCount -= 1;
+        guessCount.innerText = currentCount;
+    }
+}
+
+const updateUsedWords = function(event) {
+    const letter = event.key.toUpperCase();
+    if(addToUsedLetter(letter)) {
+        if(usedLetters.innerText.length == 0) {
+            usedLetters.innerText += letter;
+        }
+        else {
+            usedLetters.innerText += `, ${letter}`;
+        }
+    }
+}
 
 // Choosing random picture
 let randPic = choosePic(picNames);
-let randName = pictures[randPic]["name"];
-let letter = '';
+let randName = pictures[randPic]["name"].toUpperCase();
 console.log(randName);
 
 // References
 let blanks = document.querySelector('#blanks');
+let guessCount = document.querySelector('#guessCount');
+let usedLetters = document.querySelector('#usedLetters');
+let currentCount = maxGuess;
 
 currentBlanks = makeBlanks(randName);
 blanks.innerText = currentBlanks;
+guessCount.innerText = currentCount;
+usedLetters.innerText = '';
 
 document.addEventListener('keyup', updateGame);
